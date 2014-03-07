c2ccdf8a40581b2fafcb40ee057e05366d7068bd
Francois Grobbelaar <fgrob82@gmail.com>
2014-03-07 21:50:59 +1000

    Fix revision number in dmg filename

--- dmg.sh
+++ dmg.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-export PACKAGE_NAME=makehumansvn-$(hg identify -i makehuman_hg)-osx
+export PACKAGE_NAME=makehumansvn-r$(hg identify -n makehuman_hg)-osx
 
 # Package result from package.sh into a Mac OS X disk image (dmg)
 echo Creating distributable disk image
