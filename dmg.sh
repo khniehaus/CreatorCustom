f29d44d79c1585a27698129a4259712d95f0f24d
Jonas Hauquier <jonas.hauquier@makehuman.org>
2014-05-04 08:47:32 -0700

    Working fix for the -5341 error of hdiutil in OSX Mavericks. Will probably work as long as the MH app package does not approach 500MB size

--- dmg.sh
+++ dmg.sh
@@ -7,13 +7,15 @@ export PACKAGE_NAME=makehumansvn-r$(hg identify -n makehuman_hg)-osx
 
 # Work around a bug that sometimes causes error -5341 with hdiutil on
 # Mavericks
-touch dist/.Trash
+#touch dist/.Trash
+# Appears not to be enough, so we just add -size 500M as extra option
+# (MH will not approach this size anytime soon)
 
 # Package result from package.sh into a Mac OS X disk image (dmg)
 echo Creating distributable disk image
-hdiutil create $PACKAGE_NAME -srcfolder dist -volname 'MakeHuman for Mac OS X'
+hdiutil create $PACKAGE_NAME -srcfolder dist -volname 'MakeHuman for Mac OS X' -size 500M
 
-if [ ! -a ${PACKAGE_NAME}.dmg ]
+if [ ! -f "${PACKAGE_NAME}.dmg" ]
 then
 	echo "Creating DMG image failed, file not found: ${PACKAGE_NAME}.dmg"
 	exit 1
