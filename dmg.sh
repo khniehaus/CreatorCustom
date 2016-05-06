595021792094d06049d03f68b658a3df49d4796b
Francois Grobbelaar <fgrob82@gmail.com>
2016-05-06 15:06:27 +1000

    Fix dmg creation script. I've also bumped the image size to 600M as we are approaching the 500M point.

--- dmg.sh
+++ dmg.sh
@@ -7,13 +7,7 @@ cd "$( cd -P "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 export PACKAGE_NAME=makehumanhg-r$(hg identify -n makehuman_hg)-osx
 
-# Work around a bug that sometimes causes error -5341 with hdiutil on
-# Mavericks
-#touch dist/.Trash
-# Appears not to be enough, so we just add -size 500M as extra option
-# (MH will not approach this size anytime soon)
-
-if [ -n "$MAKEHUMAN_DMG_VOLNAME" ]
+if [ -z "$MAKEHUMAN_DMG_VOLNAME" ]
 then
     echo "MAKEHUMAN_DMG_VOLNAME variable not set, using default: MakeHuman for Mac OS X"
     export MAKEHUMAN_DMG_VOLNAME='MakeHuman for Mac OS X'
@@ -25,7 +19,7 @@ echo "  Output filename: ${PACKAGE_NAME}.dmg"
 
 # Package result from package.sh into a Mac OS X disk image (dmg)
 echo Creating distributable disk image
-hdiutil create $PACKAGE_NAME -srcfolder dist -volname "$MAKEHUMAN_DMG_VOLNAME" -size 500M
+hdiutil create $PACKAGE_NAME -srcfolder dist -volname "$MAKEHUMAN_DMG_VOLNAME" -size 600M
 
 if [ ! -f "${PACKAGE_NAME}.dmg" ]
 then
